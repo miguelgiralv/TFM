70e4ba7fe7772dbbf4bcbf2241eeba423788193d
unknown <miguelgiralv@gmail.com>
2024-07-11 20:18:00 +0200

    11/07/2024_1

--- scripts/NetActivity/process_tissues_final.R
+++ scripts/NetActivity/process_tissues_final.R
@@ -3,6 +3,8 @@ library(NetActivity)
 library(limma)
 library(SummarizedExperiment)
 library(tidyverse)
+library(tibble)
+
 
 path="C:/Users/Miguel/Documents/UNIVERSIDAD/6_MASTER_BIOINFORMATICA/TFM/Repositorio/TFM/" # el path al repositorio
 
@@ -113,28 +115,24 @@ for (i in 1:length(score_tissue_list)) {
   filtered_assay_data <- assay_data[!rownames(assay_data) %in% zero_sum_row_names, ]
   filtered_row_data <- rowData(score_tissue)[!rownames(rowData(score_tissue)) %in% zero_sum_row_names, ]
 
-  # Create a new SummarizedExperiment object with the filtered data
   score_tissue_filtered <- SummarizedExperiment(
     assays = list(expression = filtered_assay_data),
     colData = colData(score_tissue),
     rowData = filtered_row_data
   )
-  # Update the list with the filtered SummarizedExperiment object
   score_tissue_list[[tissue_names[[i]]]] <- score_tissue_filtered
-  # Print the number of remaining rows after filtering
   message(paste(tissue_names[[i]], "procesado, filas restantes:", nrow(filtered_assay_data)))
 }
 
 save(score_tissue_list, file = paste0(path, "results/netactivity/se_list3.RData"))
 
+# correccion en toda la tabla
 
 all_pvalues <- data.frame()
-all_info <- data.frame()
 
 for (i in 1:length(score_tissue_list)) {
   score_tissue <- score_tissue_list[[tissue_names[[i]]]]
   
-  # Set "control" as the reference level
   colData(score_tissue)$DiseaseStatus <- as.factor(colData(score_tissue)$DiseaseStatus)
   
   colData(score_tissue)$DiseaseStatus <- relevel(colData(score_tissue)$DiseaseStatus, ref = "control")
@@ -149,28 +147,159 @@ for (i in 1:length(score_tissue_list)) {
   
   fit <- lmFit(assay(score_tissue), model_score) %>% eBayes()
   
-  fit_p_val<-fit$p.value
+  topTab_tissue <- topTable(fit, coef = 2, n = Inf)
   
-  fit_p_val_df <- as.data.frame(fit_p_val)
-
-  fit_p_val_df$tissue <- tissue_names[[i]]
-  fit_p_val_df <- fit_p_val_df %>% select("DiseaseStatusAD", "tissue")
+  topTab_tissue <- rownames_to_column(topTab_tissue, var = "GO")
   
+  topTab_tissue$tissue<-tissue_names[[i]]
 
-  all_pvalues <- rbind(all_pvalues, fit_p_val_df)
+  all_pvalues <- rbind(all_pvalues, topTab_tissue)
   
   print(paste(tissue_names[[i]], "computado"))
 }
 
-all_pvalues$Bonferroni <- p.adjust(all_pvalues$DiseaseStatusAD, method = "bonferroni")  
-all_pvalues$FDR <- p.adjust(all_pvalues$DiseaseStatusAD, method = "fdr")  
+all_pvalues$Bonferroni <- p.adjust(all_pvalues$P.Value, method = "bonferroni")  
+all_pvalues$FDR <- p.adjust(all_pvalues$P.Value, method = "fdr")  
 
-all_pvalues_Bonferroni<-all_pvalues %>% select("DiseaseStatusAD", "tissue","Bonferroni")
+all_pvalues_Bonferroni<-all_pvalues %>% select(-"FDR")
 all_pvalues_Bonferroni<-all_pvalues_Bonferroni[order(all_pvalues_Bonferroni$Bonferroni), ]
 
-all_pvalues_FDR<-all_pvalues %>% select("DiseaseStatusAD", "tissue","FDR")
+all_pvalues_FDR<-all_pvalues %>% select(-"Bonferroni")
 all_pvalues_FDR <- all_pvalues_FDR[order(all_pvalues_FDR$FDR), ]
+
+head(all_pvalues_FDR)
+
+head(all_pvalues_Bonferroni)
+
 
 write.csv(all_pvalues_Bonferroni, file = paste0(path,"results/netactivity/", "bonferroni.txt"), row.names = TRUE)
 write.csv(all_pvalues_FDR, file = paste0(path,"results/netactivity/","FDR.txt"), row.names = TRUE)
+
+
+# correccion tejido por tejido
+
+all_pvalues_2 <- data.frame()
+
+for (i in 1:length(score_tissue_list)) {
+  score_tissue <- score_tissue_list[[tissue_names[[i]]]]
+  
+  colData(score_tissue)$DiseaseStatus <- as.factor(colData(score_tissue)$DiseaseStatus)
+  
+  colData(score_tissue)$DiseaseStatus <- relevel(colData(score_tissue)$DiseaseStatus, ref = "control")
+  
+  tissue_name <- tissue_names[[i]]
+
+  model_score <- model.matrix(~ DiseaseStatus, colData(score_tissue))
+  
+  fit <- lmFit(assay(score_tissue), model_score) %>% eBayes()
+  
+  topTab_tissue <- topTable(fit, coef = 2, n = Inf)
+  
+  topTab_tissue <- rownames_to_column(topTab_tissue, var = "GO")
+  
+  topTab_tissue$tissue<-tissue_names[[i]]
+  
+  topTab_tissue$Bonferroni <- p.adjust(topTab_tissue$P.Value, method = "bonferroni")  
+  
+  topTab_tissue$FDR <- p.adjust(topTab_tissue$P.Value, method = "fdr")  
+  
+  all_pvalues_2 <- rbind(all_pvalues_2, topTab_tissue)
+  
+  print(paste(tissue_names[[i]], "computado"))
+}
+
+
+all_pvalues_Bonferroni_2<-all_pvalues_2 %>% select(-"FDR")
+all_pvalues_Bonferroni_2<-all_pvalues_Bonferroni_2[order(all_pvalues_Bonferroni_2$Bonferroni), ]
+
+all_pvalues_FDR_2<-all_pvalues_2 %>% select(-"Bonferroni")
+all_pvalues_FDR_2 <- all_pvalues_FDR_2[order(all_pvalues_FDR_2$FDR), ]
+
+head(all_pvalues_FDR_2)
+head(all_pvalues_Bonferroni_2)
+
+
+write.csv(all_pvalues_Bonferroni, file = paste0(path,"results/netactivity/", "bonferroni_2.txt"), row.names = TRUE)
+write.csv(all_pvalues_FDR, file = paste0(path,"results/netactivity/","FDR_2.txt"), row.names = TRUE)
+
+
+
+## expresion de genes:
+
+
+esophagus<-score_tissue_list[["Esophagus_Muscularis"]]
+hippocampus<-score_tissue_list[["Brain_Hippocampus"]]
+heart_left<-score_tissue_list[["Heart_Left_Ventricle"]]
+
+data.frame(Expression = as.vector(assay(esophagus["GO:0002467", ])),
+           cases=esophagus$DiseaseStatus) %>%
+  ggplot(aes(x = cases, y = Expression, col = cases)) +
+  geom_boxplot() +
+  theme_bw() + 
+  ylab("NetActivity scores") +
+  ggtitle("Expresion de GO:0002467")
+
+weights <- rowData(esophagus)["GO:0002467", ]$Weights_SYMBOL[[1]]
+data.frame(weight = weights, gene = names(weights)) %>%
+  mutate(Direction = ifelse(weight > 0, "Positive", "Negative")) %>%
+  ggplot(aes(x = gene, y = abs(weight), fill = Direction)) + 
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  ylab("Weight") +
+  xlab("Gene")
+
+
+data.frame(Expression = as.vector(assay(hippocampus["GO:0051481", ])),
+           cases=hippocampus$DiseaseStatus) %>%
+  ggplot(aes(x = cases, y = Expression, col = cases)) +
+  geom_boxplot() +
+  theme_bw() + 
+  ylab("NetActivity scores") +
+  ggtitle("Expresion de GO:0051481")
+
+weights <- rowData(hippocampus)["GO:0051481", ]$Weights_SYMBOL[[1]]
+data.frame(weight = weights, gene = names(weights)) %>%
+  mutate(Direction = ifelse(weight > 0, "Positive", "Negative")) %>%
+  ggplot(aes(x = gene, y = abs(weight), fill = Direction)) + 
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  ylab("Weight") +
+  xlab("Gene")
+
+
+data.frame(Expression = as.vector(assay(heart_left["GO:0070572", ])),
+           cases=heart_left$DiseaseStatus) %>%
+  ggplot(aes(x = cases, y = Expression, col = cases)) +
+  geom_boxplot() +
+  theme_bw() + 
+  ylab("NetActivity scores") +
+  ggtitle("Expresion de GO:0070572")
+
+weights <- rowData(heart_left)["GO:0070572", ]$Weights_SYMBOL[[1]]
+data.frame(weight = weights, gene = names(weights)) %>%
+  mutate(Direction = ifelse(weight > 0, "Positive", "Negative")) %>%
+  ggplot(aes(x = gene, y = abs(weight), fill = Direction)) + 
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  ylab("Weight") +
+  xlab("Gene")
+
+  
+data.frame(Expression = as.vector(assay(heart_left["GO:0001516", ])),
+           cases=heart_left$DiseaseStatus) %>%
+  ggplot(aes(x = cases, y = Expression, col = cases)) +
+  geom_boxplot() +
+  theme_bw() + 
+  ylab("NetActivity scores") +
+  ggtitle("Expresion de GO:0001516")
+
+
+weights <- rowData(heart_left)["GO:0001516", ]$Weights_SYMBOL[[1]]
+data.frame(weight = weights, gene = names(weights)) %>%
+  mutate(Direction = ifelse(weight > 0, "Positive", "Negative")) %>%
+  ggplot(aes(x = gene, y = abs(weight), fill = Direction)) + 
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  ylab("Weight") +
+  xlab("Gene")
 
