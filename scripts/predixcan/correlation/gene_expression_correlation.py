32ca121695e3142698ec2958fbed07998e2eabfc
unknown <miguelgiralv@gmail.com>
2024-10-01 19:34:03 +0200

    TFM_final_commit

--- scripts/predixcan/correlation/gene_expression_correlation.py
+++ scripts/predixcan/correlation/gene_expression_correlation.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+
 
 path = "C:/Users/Miguel/Documents/UNIVERSIDAD/6_MASTER_BIOINFORMATICA/TFM/Repositorio/TFM"
 
@@ -26,33 +28,7 @@ for tissue in predict_files:
 # comprobamos que se ha cargado bien
 data_frames[1]
                         
-###BIEN! COMPROBAR CON CARLOS Y HACER EL LOOP POR TODOS LOS TEJIDOS PARA NO DEJARME NINGUN GEN, LUEGO HACER MERGE EN UNA                
-number=1                
-all_correlations = []
-# Iteramos por todas las columnas de los genes 
-for element in len(data_frames):
-    for col_name in data_frames[1].columns:
-        if col_name != 'IID' and col_name != 'tissue':
-            # iteramos por todos los genes y almacenamos los datos de cada gen en todos los tejidos en un df
-            gene_df = pd.DataFrame()
-            for i, df in enumerate(data_frames):
-                tissue_name = tissue_names[i]
-                if col_name in df.columns:
-                    gene_tissue = f"{col_name}_{tissue_name}"
-                    gene_df[gene_tissue] = df[col_name]       
-            # calculamos entonces la matriz de correlacion para ese gen
-            if not gene_df.empty:
-                correlation_matrix = gene_df.corr()
-                new_index = [name.split('_', 1)[-1] for name in correlation_matrix.index]
-                correlation_matrix.index = new_index
-            all_correlations.append(correlation_matrix)
-            number=number+1
-            print(number)
-
-
-df = df.loc[:, ~df.columns.duplicated()]
-
-############################ ALTERNATIVA CON TODOS LOS
+### hacemos un loop por todos los tejidos para coger todos los genes                
 # emtemos un contador para tener todos los genes distintos
 number = 1
 #  Creamos un diccionario vacio para almacenar una matriz de correlacion para cada gen
@@ -96,14 +72,13 @@ merged_df = pd.concat(flattened_correlations.values(), axis=1)
 
 transposed_df = merged_df.T
 
-# COMPROBAR QUE ES LO QUE QUERIA CARLOS Y LUEGO HACER LOOP POR TODOS LOS TEJIDOS PARA NO DEJARME NINGUN GEN, LUEGO HACER MERGE QUITANDO LOS GENES REDUNDANTES
 # pendiente
 merged_df = pd.concat(all_correlations, axis=1)
 transposed_df = merged_df.T
+# lo guardamos
 transposed_df.to_csv(f"{path}/results/predictXcan/test/vcf_1000G_hg37_mashr/correlation.txt", sep="\t", header= True)
 
 transposed_df=pd.read_csv(f"{path}/results/predictXcan/test/vcf_1000G_hg37_mashr/correlation.txt", sep="\t")
-
 def split_gene_tissue(value):
     parts = value.split('_', 1)
     gene = parts[0]
@@ -114,7 +89,8 @@ transposed_df[['Gene', 'Tissue']] = transposed_df['Unnamed: 0'].apply(lambda x: 
 
 transposed_df.drop(columns=['Unnamed: 0'], inplace=True)
 
-tissues = transposed_df.columns[1:-2]  # Adjusting for the 'Gene' and 'Tissue' columns
+# Ajustamos la columnas
+tissues = transposed_df.columns[1:-2] 
 
 mean_correlation_matrix = pd.DataFrame(index=tissues, columns=tissues)
 
@@ -137,17 +113,36 @@ mean_correlation_matrix.set_index('Unnamed: 0', inplace=True)
 sorted_columns = sorted(mean_correlation_matrix.columns)
 mean_correlation_matrix = mean_correlation_matrix[sorted_columns]
 mean_correlation_matrix = mean_correlation_matrix.sort_index()
+# quitamos las "_"
+mean_correlation_matrix.columns = mean_correlation_matrix.columns.str.replace('_', ' ')
+mean_correlation_matrix.index = mean_correlation_matrix.index.str.replace('_', ' ')
 
+mean_correlation_matrix
+
+# ahora ordenamos por los valores excluyendo la diagonal con valores 1
+np.fill_diagonal(mean_correlation_matrix.values, np.nan)
+row_sums = mean_correlation_matrix.sum(axis=1)
+col_sums = mean_correlation_matrix.sum(axis=0)
+
+sorted_indices = row_sums.sort_values(ascending=False).index
+mean_correlation_matrix_sorted = mean_correlation_matrix.loc[sorted_indices, sorted_indices]
+
+# volvemos a darle el valor 1 a la diagnoal
+np.fill_diagonal(mean_correlation_matrix_sorted.values, 1.0)
+
+print(mean_correlation_matrix_sorted)
+
+
+# creamos el heatmap
 plt.figure(figsize=(30, 30))
-ax=sns.heatmap(mean_correlation_matrix, annot=False, cmap='Reds', vmin=0, vmax=1,square=True)
+ax=sns.heatmap(mean_correlation_matrix_sorted, annot=False, cmap='Reds', vmin=0, vmax=1,square=True)
 ax.xaxis.set_label_position('top')
 ax.xaxis.tick_top()
 plt.xticks(rotation=90)  
 plt.yticks(rotation=0)  
-plt.xlabel("Tissues", fontsize=18, labelpad=40)
-plt.ylabel("Tissues", fontsize=18, labelpad=40)
+plt.xlabel("", fontsize=1, labelpad=1)
+plt.ylabel("", fontsize=1, labelpad=1)
 plt.show()
-
 
 
 ##### calculamos la media de correlacion entre tejidos:
@@ -167,7 +162,6 @@ def replace_values(row, columns):
     return row
 
 # Aplicamos a cada fila
-transposed_df2
 transposed_df2 = transposed_df2.apply(lambda row: replace_values(row, columns), axis=1)
 transposed_df2_filter = transposed_df2.drop(columns=['Unnamed: 0'])
 transposed_df2_filter = transposed_df2_filter[sorted(transposed_df2_filter.columns)]
@@ -175,76 +169,14 @@ transposed_df2_melt = transposed_df2_filter.melt(var_name='Tissue', value_name='
 
 transposed_df2_melt.to_csv(f"{path}/results/predictXcan/test/vcf_1000G_hg37_mashr/correlation_boxplot.txt", sep="\t", header= True)
 
+transposed_df2_melt=pd.read_csv(f"{path}/results/predictXcan/test/vcf_1000G_hg37_mashr/correlation_boxplot.txt", sep="\t")
+
+transposed_df2_melt['Tissue'] = transposed_df2_melt['Tissue'].str.replace('_', ' ')
+
 # y lo representamos en un boxplot
 plt.figure(figsize=(12, 8))
-sns.boxplot(x='Tissue', y='Correlation', data=transposed_df2_melt)
+sns.boxplot(x='Tissue', y='Correlation', data=transposed_df2_melt, width=0.4, color='lightblue')  # Set width and color
 plt.xticks(rotation=90)  
-plt.title('Correlation of gene expression accross Tissues', fontsize = 20)
-plt.xlabel("Tissues", fontsize=16)
-plt.ylabel("Correlation", fontsize=18)
-plt.show()
-
-
-
-
-#solo con tejido de cerebro
-brain_tissues = [tissue for tissue in mean_correlation_matrix.columns if tissue.lower().startswith('brain')]
-
-filtered_matrix = mean_correlation_matrix.loc[brain_tissues, brain_tissues]
-
-plt.figure(figsize=(12, 10))
-ax=sns.heatmap(filtered_matrix, annot=False, cmap='Reds', vmin=0, vmax=1)
-ax.xaxis.set_label_position('top')
-ax.xaxis.tick_top()
-plt.xticks(rotation=90)  
-plt.yticks(rotation=0)   
-plt.xlabel("Tissues", fontsize=18, labelpad=30)
-plt.ylabel("Tissues", fontsize=18, labelpad=30)
-plt.show()
-
-# solo con 19 tejidos:
-mean_correlation_matrix=pd.read_csv(f"{path}/results/predictXcan/test/vcf_1000G_hg37_mashr/correlation_mean_matrix.txt", sep="\t")
-
-selected_columns = [
-    "Artery_Aorta",
-    "Artery_Coronary",
-    "Artery_Tibial",
-    "Brain_Amygdala",
-    "Brain_Anterior_cingulate_cortex_BA24",
-    "Brain_Caudate_basal_ganglia",
-    "Brain_Cerebellar_Hemisphere",
-    "Brain_Cerebellum",
-    "Brain_Cortex",
-    "Brain_Frontal_Cortex_BA9",
-    "Brain_Hippocampus",
-    "Brain_Hypothalamus",
-    "Brain_Nucleus_accumbens_basal_ganglia",
-    "Brain_Putamen_basal_ganglia",
-    "Brain_Substantia_nigra",
-    "Cells_EBV-transformed_lymphocytes",
-    "Heart_Atrial_Appendage",
-    "Heart_Left_Ventricle",
-    "Whole_Blood"
-]
-
-# comprobamos que las columnas existen
-existing_columns = [col for col in selected_columns if col in mean_correlation_matrix.columns]
-print("Columns selected:", existing_columns)
-
-# Seleccionamos las columnas de los tejidos selected
-mean_correlation_matrix_filtered = mean_correlation_matrix[['Unnamed: 0'] + existing_columns]
-
-filtered_df = mean_correlation_matrix_filtered[mean_correlation_matrix_filtered['Unnamed: 0'].isin(selected_columns)]
-
-filtered_df.set_index('Unnamed: 0', inplace=True)
-filtered_df = filtered_df.sort_index()
-
-plt.figure(figsize=(12, 10))
-ax=sns.heatmap(filtered_df, annot=False, cmap='Reds', vmin=0, vmax=1)
-ax.xaxis.set_label_position('top')
-ax.xaxis.tick_top()
-plt.xticks(rotation=90)  
-plt.yticks(rotation=0)   
-plt.xlabel("Tissues", fontsize=18, labelpad=30)
-plt.ylabel("Tissues", fontsize=18, labelpad=30)
+plt.xlabel(" ", fontsize=1)
+plt.ylabel("Gene-tissue expression correlation", fontsize=18)
 plt.show()
